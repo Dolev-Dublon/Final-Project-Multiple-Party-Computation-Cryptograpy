41886e2db90d583bde4343af07021b41224bc3bc
ddublon <d.dublon@alphaomega-eng.com>
2023-06-06 23:01:03 +0300

    website added

--- index1.js
+++ index1.js
@@ -1,43 +1,24 @@
-const SubmitButton = document.querySelector("#SubmitList");
-const ListInput = document.querySelector("#ListInput");
-const result = document.querySelector("#result");
+const result = document.getElementById("data-div");
 
-// add event listener to the ListInput on input change so when it has input the submit button is no longer disabled
-ListInput.addEventListener("input", () => {
-  if (ListInput.value.trim() !== "") {
-    // Check if input is not empty
-    SubmitButton.disabled = false;
-  } else {
-    SubmitButton.disabled = true;
-  }
-});
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-SubmitButton.addEventListener("click", () => {
-  SubmitButton.style.color = "blue";
-  SubmitButton.style.backgroundColor = "red";
-  // get the value of the input
-  const ListInputValue = ListInput.value;
-  // the value is a string of 1,2,3 seperated by comma so we need to convert it to an array
-  const ListInputValueArray = ListInputValue.split(",");
-  // we need to convert the array of strings to an array of numbers
-  const ListInputValueArrayNumbers = ListInputValueArray.map((item) => {
-    return parseInt(item);
-  });
-  // we need to sort the array of numbers
-  const ListInputValueArrayNumbersSorted = ListInputValueArrayNumbers.sort(
-    (a, b) => {
-      return a - b;
-    }
-  );
+  var but = this.querySelector('[type="submit"]');
+  but.classList.toggle("sending");
+  but.blur();
   // send a POST request to the server
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:8080", true);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(ListInputValueArrayNumbersSorted));
+  xhr.send(JSON.stringify([1, 2, 5]));
   xhr.onload = () => {
     if (xhr.status === 200) {
       console.log(xhr.response);
+      setTimeout(() => {}, 2000);
       result.innerHTML = xhr.response;
+      result.className = "visible floating-div";
+      but.classList.remove("sending");
+      but.blur();
     }
   };
 });
