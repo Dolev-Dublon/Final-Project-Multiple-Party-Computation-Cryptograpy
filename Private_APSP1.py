f39b65f57148b93b7f68dbe83370e3b860370936
Aviad_Gilboa <35407628+Avgilboa@users.noreply.github.com>
2023-08-10 10:52:49 +0300

    End of dolev and Aviad part (:

--- Private_APSP1.py
+++ Private_APSP1.py
@@ -13,9 +13,36 @@ num_of_bits = 64
 64 BITS can calculate graph with 1M nodes without problem in the union graph (that need big scale of numbers)
 """
 
-def ASPS(graph):
+
+def sort_graph_edges(graph):
+    """
+    Sort the edges and nodes of a graph based on lexicographical order and return a new graph.
+    """
+    # Extract edges from the graph along with their weights
+    edges = [(u, v, graph[u][v]['weight']) for u, v in graph.edges()]
+
+    # Sort edges based on the smallest node first, then the larger node
+    sorted_edges = sorted(edges, key=lambda edge: (min(edge[0], edge[1]), max(edge[0], edge[1])))
+
+    # Create a new graph and add the sorted nodes first
+    sorted_graph = nx.Graph()
+    for node in sorted(graph.nodes()):
+        sorted_graph.add_node(node)
+
+    # Then add the sorted edges
+    for edge in sorted_edges:
+        # Ensure the smaller node is the source node
+        u, v = min(edge[0], edge[1]), max(edge[0], edge[1])
+        sorted_graph.add_edge(u, v, weight=edge[2])
+
+    return sorted_graph
+
+
+
+def ASPS(graph_):
     client_socket = init_connection_apsp1()
 
+    graph = sort_graph_edges(graph_)
     P_R_edges = []
     P_B_edges = []
     B1_edges = []
@@ -170,16 +197,16 @@ def ASPS(graph):
 
 if __name__ == "__main__":
     """ G1 """
-    graph = nx.Graph()
-    graph.add_edge("c1", "c2", weight=5)
-    graph.add_edge("c1", "c3", weight=10)
-    graph.add_edge("c3", "c4", weight=5)
-    # graph.add_edge("c1", "c5", weight=13)
-    Graph_res = ASPS(graph)
-
-    print("Nodes:", Graph_res.nodes())
-    print("Edges:", Graph_res.edges())
-    print("Edge Weights:", [(u, v, Graph_res[u][v]['weight']) for u, v in Graph_res.edges()])
+    # graph = nx.Graph()
+    # graph.add_edge("c1", "c2", weight=5)
+    # graph.add_edge("c1", "c3", weight=10)
+    # graph.add_edge("c3", "c4", weight=5)
+    # # graph.add_edge("c1", "c5", weight=13)
+    # Graph_res = ASPS(graph)
+    #
+    # print("Nodes:", Graph_res.nodes())
+    # print("Edges:", Graph_res.edges())
+    # print("Edge Weights:", [(u, v, Graph_res[u][v]['weight']) for u, v in Graph_res.edges()])
 
     # import matplotlib.pyplot as plt
     #
@@ -251,29 +278,31 @@ if __name__ == "__main__":
 
     """ G5 """
     #
-    # graph = nx.Graph()
-    # graph.add_edge("f", "e", weight=12)
-    # graph.add_edge("f", "d", weight=5)
-    # graph.add_edge("d", "c", weight=8)
-    # graph.add_edge("c", "b", weight=3)
-    # graph.add_edge("b", "a", weight=6)
-    # graph.add_edge("a", "e", weight=4)
-    # graph.add_edge("a", "f", weight=10)
-    # graph.add_edge("b", "d", weight=7)
-    #
-    #
-    #
-    # Graph_res = ASPS(graph)
-    #
-    # print("Nodes:", Graph_res.nodes())
-    # print("Edges:", Graph_res.edges())
-    # print("Edge Weights:", [(u, v, Graph_res[u][v]['weight']) for u, v in Graph_res.edges()])
-    #
-    # import matplotlib.pyplot as plt
-    #
-    # pos = nx.spring_layout(graph)
-    # labels = nx.get_edge_attributes(graph, 'weight')
-    # nx.draw(graph, pos, with_labels=True, node_size=2000, node_color='pink', font_size=15, width=3)
-    # nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, font_size=15)
-    # plt.title("Mystica Realm Magical Portals")
-    # plt.show()
+    graph = nx.Graph()
+
+
+
+    graph.add_edge("a", "b", weight=10)
+    graph.add_edge("a", "c", weight=7)
+    graph.add_edge("b", "d", weight=5)
+    graph.add_edge("d", "c", weight=6)
+    graph.add_edge("d", "e", weight=3)
+    graph.add_edge("e", "a", weight=8)
+    graph.add_edge("e", "f", weight=2)
+    graph.add_edge("f", "b", weight=9)
+
+
+    Graph_res = ASPS(graph)
+
+    print("Nodes:", Graph_res.nodes())
+    print("Edges:", Graph_res.edges())
+    print("Edge Weights:", [(u, v, Graph_res[u][v]['weight']) for u, v in Graph_res.edges()])
+
+    import matplotlib.pyplot as plt
+
+    pos = nx.spring_layout(graph)
+    labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw(graph, pos, with_labels=True, node_size=2000, node_color='pink', font_size=15, width=3)
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, font_size=15)
+    plt.title("Mystica Realm Magical Portals")
+    plt.show()
